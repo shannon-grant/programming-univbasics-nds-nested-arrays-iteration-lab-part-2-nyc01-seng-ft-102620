2aec326de4b576cef2fd7c96f19d491913d70711
Shannon Grant <grant.shannon90@gmail.com>
2020-10-01 16:03:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ def find_min_in_nested_arrays(src)
   row_index = 0
   while row_index < src.count do
     element_index = 0
-    minimum_value = 
+    minimum_value = src[row_index]element_index].min
     while element_index < src[row_index].count do
       if src[row_index][element_index]
         minimum_value = src[row_index][element_index]
